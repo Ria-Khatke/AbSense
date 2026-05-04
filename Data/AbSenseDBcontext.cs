7f9ca68f8c389d7f63fd777a614217abf534b197
Ria khatke <165070170+Ria-Khatke@users.noreply.github.com>
2026-05-04 17:22:22 +0100

    Installed EF Core, initialised Database

---
+++ Data/AbSenseDBcontext.cs
@@ -0,0 +1,11 @@
+﻿namespace AbSense.Data
+{
+    using AbSense.Models;
+    using Microsoft.EntityFrameworkCore;
+    public class AbSenseDBcontext : DbContext // inherits the abilities from dbcontext 
+    {
+        public AbSenseDBcontext(DbContextOptions<AbSenseDBcontext> options) : base(options) { }
+
+        public DbSet<staff_info> User { get; set; }
+    }
+}
