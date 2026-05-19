d131db17ee4513a9ccbc03ad5b401b6ed49830de
Ria khatke <165070170+Ria-Khatke@users.noreply.github.com>
2026-05-19 22:28:00 +0100

    Writing form request logic in dashboard controller

--- Controllers/DashboardController.cs
+++ Controllers/DashboardController.cs
@@ -40,8 +40,57 @@ namespace AbSense.Controllers
 
             ViewBag.AllowedLeaves = balance != null ? balance.TotalAllowance: 0;
 
+
+            var request = abSenseDBcontext.HolidayInfos;
+                var user_requests = request.Where (r=> r.StaffInfoId == staff_id)
+                .OrderByDescending(r=> r.CreatedAt)
+                .ToList();
+            //This let the from data create into a list and organise it by the date created and show the most recent one first
+
+            ViewBag.RemainingLeaves = balance != null ? 
+                balance.TotalAllowance - balance.UsedLeaves: 0;
+
+            return View(request);
+
+
+
             return View();
         }
+
+        [HttpPost]
+
+        public IActionResult Book_leave(DateTime StartDate, DateTime EndDate, LeaveType leaveType, string? Reason)
+        { 
+            var staffId = HttpContext.Session.GetInt32("StaffInfoId");
+
+            if (EndDate < StartDate)
+                TempData["Error"] = "End date cannot be more before start date";
+
+
+
+            var leave_request = new HolidayInfo
+            {
+                StaffInfoId = staffId.Value,
+                StartDate = StartDate,
+                EndDate = EndDate,
+                LeaveType = leaveType,
+                Reason = Reason,
+                Status = LeaveStatus.Pending,
+                CreatedAt = DateTime.Now,
+                UpdatedAt = DateTime.Now
+            };
+
+            abSenseDBcontext.HolidayInfos.Add(leave_request);
+            abSenseDBcontext.SaveChanges();
+
+            TempData["Success"] = "Leave request submitted successfully.";
+            return RedirectToAction("Staff_Dashboard");
+
+
+
+
+        }
+
 
     }
 }
